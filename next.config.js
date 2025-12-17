2dd0a6966886f4a959ab167102f0966aebd36820
Nathan Njehia <105646199+njehia-nathan@users.noreply.github.com>
2025-12-17 23:20:07 +0300

    hanges

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Silence Turbopack warning for PWA webpack config
+  turbopack: {},
 };
 
 module.exports = withPWA(nextConfig);
